68d6c1f726ffe4944fa3c1bfc4f5de745af3ee0c
kyo <jyk8929@naver.com>
2023-03-10 18:28:32 +0900

    Update 정은교_03_10.py

--- 2023_03_10/정은교_03_10.py
+++ 2023_03_10/정은교_03_10.py
@@ -1,3 +1,10 @@
+'''
+for i in range(1,10):
+    for j in range(1,10):
+        print('%d * %d = %2d' %(i, j, i*j))
+    print('----------')
+'''
+
 for i in range(2,10):
     for j in range(1,10):
         print('%d * %d = %2d\t' %(j, i, i*j), end='')
