e6cde27e8e5c3991bdcc16ecbde3a5e08942cece
seabear6400 <126859882+seabear6400@users.noreply.github.com>
2023-03-10 18:16:54 +0900

    Add files via upload

---
+++ 2023_03_10/김성우_03_10.py.py
@@ -0,0 +1,27 @@
+for i in range(1,10):
+    print('1','*',i,'=',1*i)
+print('------------')
+for i in range(1,10):
+    print('2','*',i,'=',2*i)
+print('------------')
+for i in range(1,10):
+    print('3','*',i,'=',3*i)
+print('------------')
+for i in range(1,10):
+    print('4','*',i,'=',4*i)
+print('------------')
+for i in range(1,10):
+    print('5','*',i,'=',5*i)
+print('------------')
+for i in range(1,10):
+    print('6','*',i,'=',6*i)
+print('------------')
+for i in range(1,10):
+    print('7','*',i,'=',7*i)
+print('------------')
+for i in range(1,10):
+    print('8','*',i,'=',8*i)
+print('------------')
+for i in range(1,10):
+    print('9','*',i,'=',9*i)
+print('------------')
