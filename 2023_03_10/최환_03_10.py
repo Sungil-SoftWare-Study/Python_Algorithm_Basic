d7d8613851d4cb4e94fb8250d78eb6051ef17f8f
이제석 <50763770+Suk0803@users.noreply.github.com>
2023-03-12 22:09:01 +0900

    ㅋ
    아 이거 이렇게 하는거 아닌데 ㅠㅠ

--- 2023_03_10/최환_03_10.py
+++ 2023_03_10/최환_03_10.py
@@ -1,5 +1,5 @@
 ### 일부러 미리한거 아니에요.. 제맘 아시죠 ? 0_<
 for i in range(1,10):
     for j in range(1,10):
-        print(i,"x",j,"=",i*j)
+        print(i,"x",j,"=",i*j) # print(f'{i} x {j} = {i * j})
     print("----------")
