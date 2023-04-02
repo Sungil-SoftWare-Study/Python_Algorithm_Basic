39c88358ea84127264ef1fb38ae73636d719cd1d
kyo <dmsry060209@gmail.com>
2023-04-02 20:38:55 +0900

    Update 정은교_Level3.py

--- Quiz/21220정은교/정은교_Level3.py
+++ Quiz/21220정은교/정은교_Level3.py
@@ -1,1 +1,16 @@
+num = int(input())
+result = n
+cnt = 0
 
+while True:
+    str1 = result // 10
+    str2 = result % 10
+    
+    sum = (str1 + str2) % 10
+    result = (str2 * 10) + sum
+    cnt += 1
+    
+    if(result == num):
+        break
+
+print(cnt)
