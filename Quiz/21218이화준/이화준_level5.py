c06b2a3b0969721eafaecdf302f22b987d45c8d5
LeeHwaJun <106458316+dlclfh0404@users.noreply.github.com>
2023-04-03 19:23:34 +0900

    Update 이화준_level5.py

--- Quiz/21218이화준/이화준_level5.py
+++ Quiz/21218이화준/이화준_level5.py
@@ -1,8 +1,8 @@
 def Fibonacci(n):
     if n == 0 or n == 1 :
         return n
-
-    return Fibonacci(n-1) + Fibonacci(n-2)
+   else:
+        return Fibonacci(n-1) + Fibonacci(n-2)
 
 n = int(input())
 
